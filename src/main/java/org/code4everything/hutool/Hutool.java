283b4169b7ca9ae319eab96e289e1cdc740cf5fb
easepan <easepan@qq.com>
2020-10-27 18:39:14 +0800

    feat: test

---
+++ src/main/java/org/code4everything/hutool/Hutool.java
@@ -0,0 +1,12 @@
+package org.code4everything.hutool;
+
+/**
+ * @author pantao
+ * @since 2020/10/27
+ */
+public class Hutool {
+
+    public static void main(String[] args) {
+        System.out.println("input args: " + args);
+    }
+}
