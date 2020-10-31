bed9230808c3820d5663c174fab09d63dcd3ca90
pantao <easepan@qq.com>
2020-10-31 15:51:26 +0800

    feat: build hutool

--- bin/build.py
+++ bin/build.py
@@ -1,7 +1,6 @@
 # coding:utf-8
 
 import os
-import platform
 import shutil
 
 
@@ -10,15 +9,16 @@ def build_go(os_name, arch):
     os.system('CGO_ENABLED=0 GOOS=%s GOARCH=%s go build hutool.go' %
               (os_name, arch))
     folder = '../../../target/%s' % os_name
-    os.makedirs(folder)
+    os.makedirs(folder+'/bin')
     name = 'hutool'
-    if platform.system() == 'Windows':
+    if os_name == 'windows':
         name += '.exe'
-    shutil.move(name, folder)
+    shutil.move(name, folder+'/bin')
     shutil.copy('../../../target/hutool.jar', folder)
 
 
 os.chdir('..')
+print(os.popen('git pull').read())
 print(os.popen('mvn clean package').read())
 
 os.chdir('./src/main/go')
