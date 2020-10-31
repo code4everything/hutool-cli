f7741d07d8532c8f0fd808369ac403f4e729d8bb
easepan <easepan@qq.com>
2020-10-31 15:37:53 +0800

    feat: build hutool

--- bin/build.py
+++ bin/build.py
@@ -2,19 +2,27 @@
 
 import os
 import platform
+import shutil
 
 
 def build_go(os_name, arch):
-    os.system("SET GOOS=%s" % os_name)
-    os.system('SET GOARCH=%s' % arch)
-    print('build hutool on %s' % os_name)
-    os.system('go build hutool.go')
+    print('build hutool on os[%s] for arch[%s]' % (os_name, arch))
+    os.system('CGO_ENABLED=0 GOOS=%s GOARCH=%s go build hutool.go' %
+              (os_name, arch))
+    folder = '../../../target/%s' % os_name
+    os.makedirs(folder)
+    name = 'hutool'
+    if platform.system() == 'Windows':
+        name += '.exe'
+    shutil.move(name, folder)
+    shutil.copy('../../../target/hutool.jar', folder)
 
 
 os.chdir('..')
-# print(os.popen('mvn clean package').read())
-os.system('SET CGO_ENABLED=0')
+print(os.popen('mvn clean package').read())
+
 os.chdir('./src/main/go')
+
 build_go('darwin', 'amd64')
 build_go('linux', 'amd64')
 build_go('windows', 'amd64')
