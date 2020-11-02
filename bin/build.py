9b4025939012703a4ce8805bea6df721c25652ad
pantao <easepan@qq.com>
2020-11-02 08:48:59 +0800

    fix: build go

--- bin/build.py
+++ bin/build.py
@@ -18,15 +18,16 @@ def build_go(os_name, arch):
     print('build hutool on os[%s] for arch[%s]' % (os_name, arch))
     os.system('CGO_ENABLED=0 GOOS=%s GOARCH=%s go build hutool.go' %
               (os_name, arch))
-    folder = '../../../target/%s' % os_name
-    os.makedirs(folder+'/bin')
-    name = 'hutool'
+    os.chdir('../../../target')
+    os.makedirs(os_name+'/bin')
+    name = '../src/main/go/hutool'
     if os_name == 'windows':
         name += '.exe'
-    shutil.move(name, folder+'/bin')
-    shutil.copy('../../../target/hutool.jar', folder)
-    print(os.popen('zip -r ../../../target/%s-%s.zip %s' %
-                   (os_name, version, folder)).read())
+    shutil.move(name, os_name+'/bin')
+    shutil.copy('hutool.jar', os_name)
+    print(os.popen('zip -r %s-%s.zip %s' %
+                   (os_name, version, os_name)).read())
+    os.chdir('../src/main/go')
 
 
 os.chdir('./src/main/go')
