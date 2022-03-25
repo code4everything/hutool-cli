01562d2e40b57a316e1e61c1f79977cdea53b3bb
pantao <easepan@qq.com>
2022-03-25 11:59:13 +0800

    feat: generate dart object to map

---
+++ src/main/kotlin/org/code4everything/hutool/dart/Darts.kt
@@ -0,0 +1,19 @@
+package org.code4everything.hutool.dart
+
+/// utilities for flutter dart lang
+object Darts {
+
+    @JvmStatic
+    fun object2Map(fields: String): StringBuilder {
+        val lineSep = System.lineSeparator()
+        val builder = StringBuilder().append("Map<String, Object?> toMap() {")
+        builder.append(lineSep).append("  return <String, Object?>{")
+
+        fields.split(";").map { it.trim() }.filter { it.isNotEmpty() }.forEach {
+            val name = it.split(" ").last()
+            builder.append(lineSep).append("    \"$name\": $name,")
+        }
+
+        return builder.append(lineSep).append("  };").append(lineSep).append("}")
+    }
+}
