32b603ec877f76da455a3198db180bc382288d50
pantao <easepan@qq.com>
2022-05-06 18:04:35 +0800

    feat: optimize dart

--- src/main/kotlin/org/code4everything/hutool/dart/Darts.kt
+++ src/main/kotlin/org/code4everything/hutool/dart/Darts.kt
@@ -11,11 +11,24 @@ object Darts {
         val builder = StringBuilder().append("Map<String, dynamic> toMap() {")
         builder.append(lineSep).append("  return <String, dynamic>{")
 
-        fields.split(";").map { it.split("=", limit = 2)[0].trim() }.filter { it.isNotEmpty() }.forEach {
-            val name = it.split(" ").last()
-            builder.append(lineSep).append("    \"$name\": $name,")
+        map2FieldName(fields).forEach {
+            builder.append(lineSep).append("    \"$it\": $it,")
         }
 
         return builder.append(lineSep).append("  };").append(lineSep).append("}")
+    }
+
+    @JvmStatic
+    fun constructor(fields: String): StringBuilder {
+        val builder = StringBuilder().append("{")
+        map2FieldName(fields).forEach {
+            builder.append("this.$it,")
+        }
+        return builder.append("}")
+    }
+
+    @JvmStatic
+    private fun map2FieldName(fields: String): List<String> {
+        return fields.split(";").map { it.split("=", limit = 2)[0].trim().split(" ").last() }.filter { it.isNotEmpty() }
     }
 }
