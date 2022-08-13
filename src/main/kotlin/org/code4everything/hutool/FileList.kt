7a158bbfc6373ef228f3b0374289fea6eab572e4
easepan <easepan@qq.com>
2022-08-13 18:05:51 +0800

    feat: list file find back

--- src/main/kotlin/org/code4everything/hutool/FileList.kt
+++ src/main/kotlin/org/code4everything/hutool/FileList.kt
@@ -1,11 +1,14 @@
 package org.code4everything.hutool
 
+import cn.hutool.core.comparator.ComparatorChain
+import cn.hutool.core.date.DateUtil
 import cn.hutool.core.io.FileUtil
 import cn.hutool.core.util.StrUtil
 import com.alibaba.fastjson.JSON
 import java.io.File
 import java.nio.file.Files
 import java.nio.file.attribute.BasicFileAttributes
+import java.util.Arrays
 import java.util.Date
 import java.util.regex.Pattern
 import java.util.stream.Collectors
@@ -20,6 +23,58 @@ object FileList {
     private val dateConverter by lazy { DateConverter() }
 
     private val now by lazy { Date() }
+
+    @JvmStatic
+    @IOConverter(LineSepConverter::class)
+    fun listFiles(@IOConverter(FileConverter::class) file: File): List<String> {
+        if (!FileUtil.exist(file)) {
+            return listOf("file not found!")
+        }
+
+        if (FileUtil.isFile(file)) {
+            val date = DateUtil.formatDateTime(Date(file.lastModified()))
+            val size = FileUtil.readableFileSize(file)
+            return listOf("$date  $size  ${file.name}")
+        }
+
+        val filter = MethodArg.getSubParams(Hutool.ARG, 1)
+        val files = file.listFiles { _, name ->
+            Utils.isCollectionEmpty(filter) || filter.stream().anyMatch { name.lowercase().contains(it) }
+        }
+
+        if (Utils.isArrayEmpty(files)) {
+            return emptyList()
+        }
+
+        Arrays.sort(
+            files!!, ComparatorChain.of(Comparator.comparingInt { if (it.isDirectory) 0 else 1 },
+                Comparator.comparing { it.name },
+                Comparator.comparingLong { it.lastModified() })
+        )
+
+        val list = arrayListOf<String>()
+        var maxLen = 0
+        val size = Array(files.size) { "" }
+        for (i in files.indices) {
+            size[i] = Utils.addSuffixIfNot(FileUtil.readableFileSize(files[i]).replace(" ", ""), "B")
+            val last = size[i].length - 2
+            if (!Character.isDigit(size[i][last])) {
+                // remove 'B' if has K,M...
+                size[i] = size[i].substring(0, size[i].length - 1)
+            }
+            if (size[i].length > maxLen) {
+                maxLen = size[i].length
+            }
+        }
+
+        for (i in files.indices) {
+            val date = DateUtil.formatDateTime(Date(files[i].lastModified()))
+            val fmtSize = size[i].padStart(maxLen, ' ')
+            list.add("$date  $fmtSize  ${files[i].name}")
+        }
+
+        return list
+    }
 
     @JvmStatic
     @HelpInfo(
